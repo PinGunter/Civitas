61ff44a44dff1e657dc8b5c185ad4c53950f4afc
PinGunter <salvaromero08@gmail.com>
2020-10-20 16:32:34 +0200

    empezado clase jugador java

--- P2/Civitas/src/civitas/Jugador.java
+++ P2/Civitas/src/civitas/Jugador.java
@@ -5,10 +5,28 @@
  */
 package civitas;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Salva
  */
 public class Jugador {
+   //atributos de instancia    
+    private String nombre;
+    private boolean puedeComprar;
+    private float saldo;
+    protected boolean encarcelado;
+    private int numCasillaActual;
+
+    //atributos de clase
+    protected static int CasasMax = 4;
+    protected static int CasasPorHotel = 4;
+    protected static int HotelesMax = 4;
+    protected static float PasoPorSalida = 1000;
+    protected static float PrecioLibertad = 200;
+    private static float SaldoInicial = 7500;
     
+    //atributos de referencia
+    ArrayList<TituloPropiedad> propiedades;
 }
