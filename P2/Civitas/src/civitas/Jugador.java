9ee8c7677e43d3d5b02976e9945cfcee6b68a946
PinGunter <salvaromero08@gmail.com>
2020-10-20 20:30:46 +0200

    terminada parte JAVA - [Salva]

--- P2/Civitas/src/civitas/Jugador.java
+++ P2/Civitas/src/civitas/Jugador.java
@@ -11,8 +11,9 @@ import java.util.ArrayList;
  *
  * @author Salva
  */
-public class Jugador {
-   //atributos de instancia    
+public class Jugador implements Comparable<Jugador> {
+    //atributos de instancia
+
     private String nombre;
     private boolean puedeComprar;
     private float saldo;
@@ -26,7 +27,258 @@ public class Jugador {
     protected static float PasoPorSalida = 1000;
     protected static float PrecioLibertad = 200;
     private static float SaldoInicial = 7500;
-    
+
     //atributos de referencia
     ArrayList<TituloPropiedad> propiedades;
+    Sorpresa salvoconducto;
+
+    Jugador(String nombre) {
+        this.nombre = nombre;
+        puedeComprar = false;
+        saldo = -1;
+        encarcelado = false;
+        numCasillaActual = -1;
+        propiedades = new ArrayList<TituloPropiedad>();
+        salvoconducto = null;
+    }
+
+    protected Jugador(Jugador otro) {
+        this.nombre = otro.nombre;
+        this.puedeComprar = otro.puedeComprar;
+        this.saldo = otro.saldo;
+        this.encarcelado = otro.encarcelado;
+        this.numCasillaActual = otro.numCasillaActual;
+        this.propiedades = otro.propiedades;
+        this.salvoconducto = otro.salvoconducto;
+    }
+
+    protected boolean debeSerEncarcelado() {
+        boolean carcel = !encarcelado;
+        if (carcel) {
+            if (tieneSalvoconducto()) {
+                perderSalvoconducto();
+                Diario.getInstance().ocurreEvento("Jugador " + nombre + " se libra de la carcel");
+                carcel = false;
+            }
+        }
+        return carcel;
+    }
+
+    boolean encarcelar(int numCasillaCarcel) {
+        if (debeSerEncarcelado()) {
+            moverACasilla(numCasillaCarcel);
+            encarcelado = true;
+            Diario.getInstance().ocurreEvento("Jugador " + nombre + " va la carcel");
+        }
+        return encarcelado;
+    }
+
+    boolean obtenerSalvoconducto(Sorpresa s) {
+        boolean obtiene = !encarcelado;
+        if (obtiene) {
+            salvoconducto = s;
+            salvoconducto.tipo = TipoSorpresa.SALIRCARCEL;
+        }
+        return obtiene;
+    }
+
+    private void perderSalvoconducto() {
+        salvoconducto.usada();
+        salvoconducto = null;
+    }
+
+    boolean tieneSalvoconducto() {
+        return salvoconducto != null;
+    }
+
+    boolean puedeComprarCasilla() {
+        puedeComprar = !encarcelado;
+        return puedeComprar;
+    }
+
+    boolean paga(float cantidad) {
+        return modificarSaldo(-1f * cantidad);
+    }
+
+    boolean pagaImpuesto(float cantidad) {
+        if (encarcelado) {
+            return false;
+        } else {
+            return paga(cantidad);
+        }
+    }
+
+    boolean pagaAlquiler(float cantidad) {
+        return pagaImpuesto(cantidad);
+    }
+
+    boolean recibe(float cantidad) {
+        if (encarcelado) {
+            return false;
+        } else {
+            return modificarSaldo(cantidad);
+        }
+    }
+
+    boolean modificarSaldo(float cantidad) {
+        this.saldo += cantidad;
+        Diario.getInstance().ocurreEvento("Se incrementa el saldo del jugador: " + nombre + " en " + cantidad);
+        return true;
+    }
+
+    boolean moverACasilla(int numCasilla) {
+        if (encarcelado) {
+            return false;
+        } else {
+            numCasillaActual = numCasilla;
+            puedeComprar = false;
+            Diario.getInstance().ocurreEvento("Se mueve jugador: " + nombre + " a casilla: " + numCasilla);
+            return true;
+        }
+    }
+
+    private boolean puedoGastar(float precio) {
+        if (encarcelado) {
+            return true;
+        } else {
+            return saldo >= precio;
+        }
+    }
+
+    boolean vender(int ip) {
+        boolean vendido = false;
+        if (!encarcelado) {
+            if (existeLaPropiedad(ip)) {
+                if (propiedades.get(ip).vender(this)) {
+                    propiedades.remove(ip);
+                    Diario.getInstance().ocurreEvento("Se vende propiedad " + propiedades.get(ip).getNombre() + " del jugador " + nombre);
+                    vendido = true;
+                }
+            }
+        }
+        return vendido;
+    }
+
+    boolean tieneAlgoQueGestionar() {
+        return propiedades.size() > 0;
+    }
+
+    private boolean puedeSalirCarcelPagando() {
+        return saldo >= PrecioLibertad;
+    }
+
+    boolean salirCarcelPagando() {
+        boolean sale = false;
+        if (encarcelado && puedeSalirCarcelPagando()) {
+            paga(PrecioLibertad);
+            encarcelado = false;
+            Diario.getInstance().ocurreEvento("Jugador " + nombre + " sale de la carcel pagando " + PrecioLibertad);
+            sale = true;
+        }
+        return sale;
+    }
+
+    boolean salirCarcelTirando() {
+        boolean sale = Dado.getInstance().salgoDeLaCarcel();
+        if (sale) {
+            encarcelado = false;
+            Diario.getInstance().ocurreEvento("Jugador " + nombre + " sale de la carcel tirando los dados");
+        }
+        return sale;
+    }
+
+    boolean pasaPorSalida() {
+        modificarSaldo(PasoPorSalida);
+        Diario.getInstance().ocurreEvento("Jugador: " + nombre + " pasa por salida");;
+        return true;
+    }
+
+    public int compareTo(Jugador otro) {
+        return Float.compare(saldo, otro.saldo);
+    }
+
+    int cantidadCasasHoteles() {
+        int total = 0;
+        for (int i = 0; i < propiedades.size(); i++) {
+            total += propiedades.get(i).cantidadCasasHoteles();
+        }
+        return total;
+    }
+
+    boolean enBancarrota() {
+        return saldo < 0;
+    }
+
+    private boolean existeLaPropiedad(int ip) {
+        return ip >= 0 && ip < propiedades.size();
+    }
+
+    private int getCasasMax() {
+        return CasasMax;
+    }
+
+    int getCasasPorHotel() {
+        return CasasPorHotel;
+    }
+
+    private int getHotelesMax() {
+        return HotelesMax;
+    }
+
+    protected String getNombre() {
+        return nombre;
+    }
+
+    int getNumCasillaActual() {
+        return numCasillaActual;
+    }
+
+    private float getPrecioLibertad() {
+        return PrecioLibertad;
+    }
+
+    private float getPremioPasoSalida() {
+        return PasoPorSalida;
+    }
+
+    protected ArrayList<TituloPropiedad> getPropiedades() {
+        return propiedades;
+    }
+
+    boolean getPuedeComprar() {
+        return puedeComprar;
+    }
+
+    protected float getSaldo() {
+        return saldo;
+    }
+
+    public boolean isEncarcelado() {
+        return encarcelado;
+    }
+
+    private boolean puedoEdificarCasa(TituloPropiedad propiedad) {
+        return saldo >= propiedad.getPrecioEdificar() && propiedad.getNumCasas() < CasasMax;
+    }
+
+    private boolean puedoEdificarHotel(TituloPropiedad propiedad) {
+        return saldo >= propiedad.getPrecioEdificar() && propiedad.getNumCasas() < HotelesMax;
+    }
+
+    @Override
+    public String toString() {
+        String resultado = "Nombre: " + nombre
+                + "\nPuede Comprar: " + puedeComprar
+                + "\nSaldo: " + saldo
+                + "\nEncarcelado: " + encarcelado
+                + "\nNumCasillaActual: " + numCasillaActual
+                + "\nCasasMax: " + CasasMax
+                + "\nHotelesMax: " + HotelesMax
+                + "\nPaso por Salida: " + PasoPorSalida
+                + "\nPrecio Libertad: " + PrecioLibertad
+                + "\nSaldo inicial: " + SaldoInicial
+                + "\nPropiedades: " + propiedades
+                + "\nSalvoconducto: " + salvoconducto;
+        return resultado;
+    }
 }
