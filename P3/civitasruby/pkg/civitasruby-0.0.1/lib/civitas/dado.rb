b28d42143cf5dfaed3c5bab32901445a4804f5cf
PinGunter <salvaromero08@gmail.com>
2020-11-21 23:54:47 +0100

    ahora el main ahora peta solo a los 2 segundos

---
+++ P3/civitasruby/pkg/civitasruby-0.0.1/lib/civitas/dado.rb
@@ -0,0 +1,56 @@
+#encoding:utf-8
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+require 'singleton'
+
+module Civitas
+  class Dado
+    include Singleton
+
+    attr_reader :ultimo_resultado
+
+    @@salida_carcel = 5
+
+    def initialize
+      @random = Random.new
+      @ultimo_resultado
+      @debug = false
+    end
+
+    def tirar
+      tirada = @random.rand(6)+1
+      if @debug
+        tirada = 1
+      end
+      @ultimo_resultado = tirada
+      return tirada
+    end
+
+    def salgo_de_la_carcel
+      tirada = tirar
+      @ultimo_resultado = tirada
+      if tirada >= @@salida_carcel
+        return true
+      else
+        return false
+      end
+    end
+
+    def quien_empieza(n)
+      return @random.rand(n)
+    end
+
+    def set_debug(d)
+      @debug = d
+      if @debug
+        Diario.instance.ocurre_evento("Activado modo debug de Dado")
+      else
+        Diario.instance.ocurre_evento("Desactivado modo debug del Dado")
+      end
+    end
+
+    private :initialize
+  end
+end
---
+++ P3/civitasruby/pkg/civitasruby-0.0.1/lib/civitas/salidas_carcel.rb
@@ -0,0 +1,14 @@
+#encoding:utf-8
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+# Salva Romero
+
+module Civitas
+  module Salidas_carcel
+    PAGANDO = :pagando
+    TIRANDO = :tirando
+    Lista_salidas_carcel = [Salidas_carcel::PAGANDO, Salidas_carcel::TIRANDO]
+  end
+end
